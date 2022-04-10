f4f51b834eb624893e6284f8acfb919da23d3204
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-04-10 23:54:12 +0800

    change code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cool = require('cool-ascii-faces');
 const express = require('express');
 const app = express();
 const cors = require("cors");
-// const path = require('path');
+const path = require('path');
 const database = require('./database.js')
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -10,12 +10,12 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+//   next();
+// });
 
 express()
 .use(express.static(path.join(__dirname, 'public')))
