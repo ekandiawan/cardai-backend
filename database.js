faea0a15e8f53500eceea7810953ebdfe0ceccd8
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-04-11 00:12:16 +0800

    change env value

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL1,
   ssl: {
     rejectUnauthorized: false
   }
