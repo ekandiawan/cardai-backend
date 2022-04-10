0dca42d359fd7587e21772e174574b01e13c8620
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-04-10 23:47:27 +0800

    revamp API

---
+++ database.js
@@ -0,0 +1,119 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// User query - all tested and worked (15 Mar)
+const getUsers = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM public."Users" ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+const createUser = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { firstName, lastName, phoneNumber, email, countryCode } = body
+    pool.query('INSERT INTO public."Users" ("firstName", "lastName", "phoneNumber", "email", "countryCode") VALUES ($1, $2, $3, $4, $5)', [firstName, lastName, phoneNumber, email, countryCode], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new user has been added.`)
+    })
+  })
+}
+
+const updateUser = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { firstName, lastName, phoneNumber, email, countryCode, id } = body
+    pool.query('UPDATE public."Users" SET "firstName" = $1, "lastName" = $2, "phoneNumber" = $3, "email" = $4, "countryCode" = $5 WHERE id = $6', [firstName, lastName, phoneNumber, email, countryCode, id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`User details have been updated.`)
+    })
+  })
+}
+
+const deleteUser = (params) => {
+  return new Promise(function(resolve, reject) {
+    const { id } = params;
+    pool.query('DELETE FROM public."Users" WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`User deleted with ID: ${id}`)
+    })
+  })
+}
+
+// UserCard query - done
+const getUserCards = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { userID } = body;
+    pool.query('SELECT * FROM public."UserCard" WHERE "userID" = $1 ORDER BY "userID" ASC', [userID], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const createUserCard = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { email, cardName } = body
+    pool.query('INSERT INTO public."UserCard" ("userID", "cardID") VALUES ((SELECT id FROM public."Users" WHERE "email" = $1), (SELECT id FROM public."Cards" WHERE "cardName" = $2))', [email, cardName], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`New card has been added.`)
+    })
+  })
+}
+
+const deleteUserCard = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { email, cardName } = body;
+    pool.query('DELETE FROM public."UserCard" WHERE "userID" IN (SELECT id FROM public."Users" WHERE "email" = $1) AND "cardID" IN (SELECT id FROM public."Cards" WHERE "cardName" = $2)', [email, cardName], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`Card deleted.`)
+    })
+  })
+}
+
+// GET merchants based on location - done!
+const getMerchantLocation = (query) => {
+  return new Promise(function(resolve, reject) {
+    const {location} = query;
+    pool.query(`SELECT m."merchantName", ml."buildingName", ml."address", ml."phoneNumber" FROM public."MerchantLocation" AS ml LEFT JOIN public."Merchants" AS m ON ml."merchantID" = m."id" LEFT JOIN public."Locations" AS l ON ml."locationID" = l."id" WHERE l."locationName" = $1`, [location], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+     resolve(results.rows);
+    })
+  }) 
+}
+
+module.exports = {
+  pool,
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  getUserCards,
+  createUserCard,
+  deleteUserCard,
+  getMerchantLocation
+};
--- index.js
+++ index.js
@@ -1,33 +1,129 @@
 const cool = require('cool-ascii-faces');
 const express = require('express');
-const path = require('path');
+const app = express();
+const cors = require("cors");
+// const path = require('path');
+const database = require('./database.js')
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/db', async (req, res) => {
+//     try {
+//       const client = await pool.connect();
+//       const result = await client.query('SELECT * FROM test_table');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.render('pages/db', results );
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   })
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  app.get('/', (request, response) => {
+    response.send(request.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+  });
+  
+  // User API
+  app.get('/user', (req, res) => {
+    database.getUsers()
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
   })
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  
+  app.post('/user/add', (req, res) => {
+    database.createUser(req.body)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+
+  app.put('/user/edit', (req, res) => {
+    database.updateUser(req.body)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+  
+  app.delete('/user/remove/:id', (req, res) => {
+    database.deleteUser(req.params)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+
+  // UserCard API
+  app.get('/user-card', (req, res) => {
+    database.getUserCards(req.body)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+  
+  app.post('/user-card/add', (req, res) => {
+    database.createUserCard(req.body)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+  
+  app.delete('/user-card/remove', (req, res) => {
+    database.deleteUserCard(req.body)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+
+  // merchant based on location API
+  app.get('/merchant-by-', (req, res) => {
+    database.getMerchantLocation(req.query)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  });
+
+app.listen(port, () => {
+    console.log(`server has started on port ${port}.`)
+});
