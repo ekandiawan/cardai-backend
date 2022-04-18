4ba12ef75fabd47bd603744e19c8b3dc8147d9dc
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-04-18 11:59:50 +0800

    delete js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 // const database = require('./database.js')
 const port = process.env.PORT || 5000;
-const db = require('./database.js')
+const db = require('./database')
 require('dotenv').config();
 
 // app.use(cors());
