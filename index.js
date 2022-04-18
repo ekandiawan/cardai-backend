eff75841e650fdbcf6e1ecbd0afad461b6409e0e
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-04-18 12:05:52 +0800

    change again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 // const database = require('./database.js')
 const port = process.env.PORT || 5000;
-const db = require('./database')
+const db = require('./database.js')
 require('dotenv').config();
 
 // app.use(cors());
