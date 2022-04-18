0775c65b1a0f084b54fd54bebfea1c2e56a6aa8a
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-04-18 14:38:35 +0800

    redefine env

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const { Pool } = require('pg');
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = process.env.HEROKU_DATABASE_URL;
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.HEROKU_DATABASE_URL : connectionString,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
 })
 
