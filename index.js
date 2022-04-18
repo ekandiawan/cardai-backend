914ecf6f9d1f762c7376790740bf498032ecb69a
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-04-18 13:49:21 +0800

    trying again

--- index.js
+++ index.js
@@ -34,18 +34,6 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .get('/user', async (req, res) => {
-    try {
-      const client = await pool.connect();
       const result = await client.query('SELECT * FROM Users');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
