69a7344dce465df7c6c5eb91dd7a3c4cd6f08b50
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-04-18 14:58:18 +0800

    revision

--- database.js
+++ database.js
@@ -1,23 +1,14 @@
 require('dotenv').config()
 const { Pool } = require('pg');
-const isProduction = process.env.NODE_ENV === 'production'
-
-const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
-})
-
-/*
-const pool = new Pool({
-  connectionString: process.env.HEROKU_DATABASE_URL + "sslmode=require",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-
+/*
 const getUsers = (request, response) => {
   pool.query(`SELECT * FROM public."Users" ORDER BY id ASC`, (error, results) => {
     if (error) {
