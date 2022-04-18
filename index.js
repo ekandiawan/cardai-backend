06ce1865a78bde7bf3bcf260da6793b8f99076b4
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-04-18 13:01:38 +0800

    testing connection 2

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ express()
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM Users');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/user', results );
+      res.render('pages/db', results );
       client.release();
     } catch (err) {
       console.error(err);
